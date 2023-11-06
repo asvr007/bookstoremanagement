fe0f88ea67db950dc3a3a38d2dc98a5800d21800
Adem Sreenivasula Reddy <sreenureddyadem@gmail.com>
2023-11-06 12:41:34 +0530

    Update db.js

--- book-main/config/db.js
+++ book-main/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://127.0.0.1:27019/book-store');
+mongoose.connect('mongodb://127.0.0.1:27017/book-store');
 
 const db = mongoose.connection;
 
